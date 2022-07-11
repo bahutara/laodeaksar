489fd96d204c8ec93ec2278f4d26e717e57a8525
bahutara <akhsaras23@gmail.com>
2022-07-11 11:56:06 +0000

    first commit

---
+++ apps/docs/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@laodeaksar/core";
+
+export default function Docs() {
+  return (
+    <div>
+      <h1>laodeaksar Documentation</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ apps/spa/src/App.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@laodeaksar/core";
+import { Thing } from "@laodeaksar/theme-kit";
+import "@laodeaksar/theme-kit/dist/tailwind.css";
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Thing />
+        <Button />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
